c48156b4733028e205b57a234c3bf2dc0b984176
YoshYoshiYosh <yoshikik@live.jp>
2019-03-31 14:39:44 +0900

    不必要なものはコメントアウトしました

--- public/script.js
+++ public/script.js
@@ -1,29 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let form = document.querySelector('form')
-  let messages = document.querySelector('#messages')
+  const form = document.querySelector('form');
+  const messages = document.querySelector('#messages');
 
   form.onsubmit = async (e) => {
-    alert('チェック1')
-    e.preventDefault()
-    let formData = new FormData()
-    let message = form.message.value
-    if (message === '') return
-    alert('チェック2')
-    formData.append('message', message)
+    e.preventDefault();
+    const formData = new FormData();
+    const message = form.message.value;
+    if (message === '') return;
+    formData.append('message', message);
     const row = await fetch(location.href, {
       method: "post",
       body: formData
-    })
-
-    alert(row.status)
+    });
     
     if (row.status === 200) {
-      const newMessage = document.createTextNode(message)
-      const newMessageElement = document.createElement("li")
-      newMessageElement.appendChild(newMessage)
-      messages.appendChild(newMessageElement)
+      const newMessage = document.createTextNode(message);
+      const newMessageElement = document.createElement("li");
+      newMessageElement.appendChild(newMessage);
+      messages.appendChild(newMessageElement);
     }
-  }
-
-  }
-)
+  };
+});
