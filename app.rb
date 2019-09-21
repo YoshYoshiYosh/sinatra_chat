983acd03676e48d55cc052e9c3b2b9db36464cb0
YoshYoshiYosh <yoshikik@live.jp>
2019-09-21 23:18:33 +0900

    Update app.rb

--- app.rb
+++ app.rb
@@ -8,8 +8,6 @@ class Chat < ActiveRecord::Base
   validates_presence_of :text
 end
 
-#reverseメソッドはruby側の処理になるのd、えactive record側でやったほうがいい
-
 get '/' do
   # @chats = Chat.all.order.reverse[0...30]
   # @chats = Chat.all.reverse_order.limit(30)
