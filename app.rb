76ffb0f5095c1d532891febe64b2a8394af2c9be
YoshYoshiYosh <yoshikik@live.jp>
2019-09-25 00:04:46 +0900

    [usersテーブル作成]

--- app.rb
+++ app.rb
@@ -8,13 +8,21 @@ class Chat < ActiveRecord::Base
   validates_presence_of :text
 end
 
+class Server < Sinatra::Base
+  enable :sessions
+end
+
 get '/' do
-  erb :root
+  if session[:id] == nil
+    erb :login
+  else
+    erb :mkroom
+  end
 end
 
 get '/:room' do
   @chats = Chat.where(room: params[:room]).last(30)
-  erb :index
+  erb :room
 end
 
 post '/:room' do
